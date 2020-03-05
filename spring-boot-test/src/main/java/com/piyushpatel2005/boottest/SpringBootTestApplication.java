8b7d663773b58b95effe6cd9137d453b32919cd8
Piyush Patel <piyushpatel2005@gmail.com>
2020-03-04 22:32:45 -0500

    logback example file

--- spring-boot-test/src/main/java/com/piyushpatel2005/boottest/SpringBootTestApplication.java
+++ spring-boot-test/src/main/java/com/piyushpatel2005/boottest/SpringBootTestApplication.java
@@ -1,13 +1,20 @@
 package com.piyushpatel2005.boottest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
 
+	private final static Logger log = LoggerFactory.getLogger(SpringBootTestApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTestApplication.class, args);
+		log.debug("Hello logger");
+
+		SpringApplication.run(
+			SpringBootTestApplication.class, args);
 	}
 
 }
