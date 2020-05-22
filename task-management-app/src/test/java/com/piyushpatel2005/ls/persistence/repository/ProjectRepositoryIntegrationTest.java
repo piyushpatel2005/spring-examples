b28faf2b102e853b98bd6c14d4d01e6d050fbab8
Piyush Patel <piyushpatel2005@gmail.com>
2020-05-21 22:56:56 -0400

    PagingAndSortingRepository -
    Paging and sorting can be achieved to limit the number of entities retrieved for a request. Such details can be controlled by implementing PagingAndSortingRepository. This extends CrudRepository so all the build-in methods are still available for the extending Repository interface.

--- task-management-app/src/test/java/com/piyushpatel2005/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
+++ task-management-app/src/test/java/com/piyushpatel2005/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
@@ -2,17 +2,24 @@ package com.piyushpatel2005.ls.persistence.repository;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 import com.piyushpatel2005.ls.persistence.model.Project;
 
@@ -31,7 +38,7 @@ public class ProjectRepositoryIntegrationTest {
 
     @Test
     public void givenProject_whenFindById_thenSuccess() {
-        Project newProject = new Project(2l, randomAlphabetic(6), LocalDate.now());
+        Project newProject = new Project(randomAlphabetic(6), LocalDate.now());
         projectRepository.save(newProject);
 
         Optional<Project> retrievedProject = projectRepository.findById(newProject.getId());
@@ -62,6 +69,26 @@ public class ProjectRepositoryIntegrationTest {
 
         List<Project> retreivedProjects = projectRepository.findByDateCreatedBetween(LocalDate.now().minusDays(1), LocalDate.now().plusDays(1));
         assertThat(retreivedProjects, hasItems(newProject, newProject2));
+    }
+    
+    @Test
+    public void givenDataCreated_whenFindAllSort_thenSuccess() {
+        List<Project> retrievedProjects = (List<Project>) projectRepository.findAll(Sort.by(Order.asc("name")));
+        
+        List<Project> sortedProjects = retrievedProjects.stream().collect(Collectors.toList());
+        sortedProjects.sort(Comparator.comparing(Project::getName));
+        
+        assertEquals(sortedProjects, retrievedProjects);
+    }
+    
+    @Test
+    public void givenDataCreated_whenFindAllPaginatedAndSort_thenSuccess() {
+        Iterable<Project> retrievedProjects = projectRepository.findAll(PageRequest.of(0, 2,Sort.by(Order.asc("name"))));
+        
+        List<Project> projectList = new ArrayList<>();
+        retrievedProjects.forEach(projectList::add);
+        
+        assertThat(projectList, hasSize(2));
     }
 
 }
